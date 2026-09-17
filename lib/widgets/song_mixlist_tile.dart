a90e16eb1a78ac500403cb6e171d4a647d04478a
Seco Vlad <seco.vlad@gmail.com>
2026-09-17 20:45:32 +0300

    Animate song mixlist tile in album screen

--- lib/widgets/song_mixlist_tile.dart
+++ lib/widgets/song_mixlist_tile.dart
@@ -3,9 +3,6 @@ import 'package:mixlists_project/models/view_models/mixlist_summary.dart';
 import 'package:mixlists_project/widgets/album_art_thumbnail.dart';
 import 'package:mixlists_project/widgets/text_styles.dart';
 
-/// This one you may want to modify to behave like the one that's in use
-/// over in the main mixlists detail screen i.e. with the nice little
-/// pop-down animation.
 class SongMixlistTile extends StatefulWidget {
   const SongMixlistTile({
     super.key,
@@ -28,8 +25,49 @@ class SongMixlistTile extends StatefulWidget {
   State<SongMixlistTile> createState() => _SongMixlistTileState();
 }
 
-class _SongMixlistTileState extends State<SongMixlistTile> {
+class _SongMixlistTileState extends State<SongMixlistTile>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  late final Animation<double> _revealAnimation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeOut,
+    reverseCurve: Curves.easeIn,
+  );
+
+  late final Animation<double> _popAnimation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeOutBack,
+    reverseCurve: Curves.easeIn,
+  );
+
+  late final Animation<double> _fadeAnimation = CurvedAnimation(
+    parent: _controller,
+    curve: const Interval(0.3, 1.0, curve: Curves.easeIn),
+    reverseCurve: const Interval(0.0, 0.7, curve: Curves.easeOut),
+  );
+
   bool _isExpanded = false;
+
+  void _toggleExpanded() {
+    setState(() => _isExpanded = !_isExpanded);
+    _isExpanded ? _controller.forward() : _controller.reverse();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,25 +116,48 @@ class _SongMixlistTileState extends State<SongMixlistTile> {
                     ],
                   ),
                 )
-              : Chip(label: Text('${mixlists.length} mixlists')),
+              : ActionChip(
+                  label: Text('${mixlists.length} mixlists'),
+                  onPressed: _toggleExpanded,
+                ),
           onTap: hasSingleMixlist
               ? () => widget.onOpenMixlist(mixlists.first.id, widget.songId)
-              : () => setState(() => _isExpanded = !_isExpanded),
+              : _toggleExpanded,
         ),
-        if (!hasSingleMixlist && _isExpanded)
-          Padding(
-            padding: const EdgeInsets.only(left: 32, right: 16, bottom: 8),
-            child: Column(
-              crossAxisAlignment: .start,
-              children: mixlists
-                  .map(
-                    (mixlist) => MixlistRow(
-                      mixlist: mixlist,
-                      onTap: () =>
-                          widget.onOpenMixlist(mixlist.id, widget.songId),
+        if (!hasSingleMixlist)
+          Align(
+            alignment: .topRight,
+            child: SizeTransition(
+              sizeFactor: _revealAnimation,
+              alignment: .bottomLeft,
+              child: ScaleTransition(
+                scale: _popAnimation,
+                alignment: .topRight,
+                child: FadeTransition(
+                  opacity: _fadeAnimation,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 32,
+                      right: 16,
+                      bottom: 8,
                     ),
-                  )
-                  .toList(),
+                    child: Column(
+                      crossAxisAlignment: .start,
+                      children: mixlists
+                          .map(
+                            (mixlist) => MixlistRow(
+                              mixlist: mixlist,
+                              onTap: () => widget.onOpenMixlist(
+                                mixlist.id,
+                                widget.songId,
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
       ],
